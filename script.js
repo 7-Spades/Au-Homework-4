8a4a13c7d3132e43b659115a6e3172b00883d546
7-Spades <edwin5106@gmail.com>
2020-01-17 16:19:49 -0600

    added answer choices function

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var Q= document.querySelector("#Question");
-var A= document.querySelector("#Answer");
 var title= document.querySelector("#TemH1");
 var paragraph= document.querySelector("#TemP");
-var starup= document.querySelector("#TemB");
+var startup= document.querySelector("#TemB");
+var position= document.querySelector("#choice");
+var Questionare= ["Who was the Second president of the United States?", "When was the Louisiana Purchase bought?", "When did U.S Civil War start?"];
 
 function begin(){
     event.preventDefault();
     title.remove();
     paragraph.remove();
-    starup.remove();
-    Quest();
+    startup.remove();
+    setup();
 };
 
-starup.addEventListener("click", begin);
+function setup(){
+    for ( var i= 0; i<4; i++){
+        var Ans= document.createElement("button");
+        Ans.setAttribute("class", "btn btn-outline-primary col p-4 mb-4");
+        Ans.setAttribute("id", i);
+        position.appendChild(Ans);
+   };
+};
+
+startup.addEventListener("click", begin);
